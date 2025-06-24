c14c36fbba6beb796729bfc5b893f6516c5c3dfd
MrKtosiek <jezierski.filip@wp.pl>
2025-06-24 15:12:51 +0200

    UGR-44 profile(feat): Add mock game history table in profile page, fix rounded table corners

--- frontend/src/pages/user.tsx
+++ frontend/src/pages/user.tsx
@@ -16,6 +16,36 @@ const User: React.FC = () => {
 					<h2>{accountService.getCurrentUser()?.nickname}</h2>
 					<p>Email: {accountService.getCurrentUser()?.email}</p>
 					<p>Role: {accountService.getCurrentUser()?.role}</p>
+					<p>Average score: 1320,23</p>
+					<h3>Games played:</h3>
+					<table className={styles.games_table}>
+            <thead>
+							<tr>
+								<th>Date</th>
+								<th>Score</th>
+							</tr>
+            </thead>
+						<tbody>
+							{/* {gamesPlayed.map((game) => (
+								<tr key={game.id}>
+									<td>{game.date}</td>
+									<td>{game.score}</td>
+								</tr>
+							))} */}
+							<tr>
+								<td>24.06.2025 16:42</td>
+								<td>2137,69</td>
+							</tr>
+							<tr>
+								<td>22.06.2025 13:37</td>
+								<td>420,37</td>
+							</tr>
+							<tr>
+								<td>20.04.2025 4:32</td>
+								<td>1519,12</td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</>
